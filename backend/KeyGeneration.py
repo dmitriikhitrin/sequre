36a3af8c97410396813148f11c7bdd1a07598f4a
dmitriikhitrin <dmitrii.khitrin@duke.edu>
2023-02-28 15:00:15 -0500

    changed the return in getPrivateKeys function

--- backend/KeyGeneration.py
+++ backend/KeyGeneration.py
@@ -12,7 +12,7 @@ def binaryToDecimal(binary):
         decimal = decimal + dec * pow(2, i)
         binary = binary//10
         i += 1
-    print(decimal)
+    return decimal
 
 def createCircuit(numUsers, QECQubits, registers):
     users = QuantumRegister(numUsers, "user")
@@ -34,8 +34,7 @@ def enterDialog(userIn, userEn):
     qc.cnot(userIn, userEn)
     return qc
 
-def closeDialog(userIn, userEn):
-    qc = enterDialog(userIn, userEn)
+def closeDialog(qc, userIn, userEn):
     qc.reset(userIn)
     qc.reset(userEn)
     return qc
@@ -48,13 +47,15 @@ def errorX(qc, userIn, userEn):
 def errorCorrection(qc, userIn, userEn, QECQubit):
     qc.cnot(userIn, QECQubit)
     qc.cnot(userEn, QECQubit)
-    qc.x(QECQubit)
     return qc
 
 def getPrivateKeys(userIn, userEn, QECQubit, keyLength):
     qc = enterDialog(userIn, userEn)
     
+    qc.barrier()
     qc = errorX(qc, userIn, userEn)
+    qc.barrier()
+    
     qc = errorCorrection(qc, userIn, userEn, QECQubit)
     qc.barrier()
     qc.measure([userIn, userEn, QECQubit], [0,1,2])
@@ -63,43 +64,32 @@ def getPrivateKeys(userIn, userEn, QECQubit, keyLength):
     lstEn = []
     counts = []
     
-    qc1 = qc
     
-    for i in range(keyLength):
+    while len(lstEn) <= keyLength:
         backend = Aer.get_backend('qasm_simulator')
         job_sim = execute(qc, backend, shots = 1)
         result = job_sim.result()
         count = result.get_counts(qc)
         counts.append(count)
-        if (int(list(count.keys())[0][0]) == 1):
+        if (int(list(count.keys())[0][0]) == 0):
             lstIn.append(list(count.keys())[0][registers-userIn-1])
             lstEn.append(list(count.keys())[0][registers-userEn-1])
-    
-#     print(counts)
 
-    # just for histogram ----------------------------
-    backend = Aer.get_backend('qasm_simulator')
-    job_sim = execute(qc, backend, shots = 1024)
-    result = job_sim.result()
-    count1 = result.get_counts(qc)
-    # -----------------------------------------------
     
     if (len(lstIn) != 0 and len(lstEn) != 0):
+        if lstIn[0] == 0:
+            lstIn = lstIn[1:]
+        if lstEn[0] == 0:
+            lstEn = lstEn[1:]
         keyIn = binaryToDecimal(int("".join(lstIn)))
         keyEn = binaryToDecimal(int("".join(lstEn)))
-    else: return "nothing has been generated"
+
+    return keyIn, keyEn
     
     
 #     qc.draw('mpl')
 #     plot_histogram(count)
-    return (keyIn, keyEn), qc1, count1
+    return (keyIn, keyEn)
 
-result = getPrivateKeys(0, 1, 3, 128)
-# keys = result[0]
-# print(keys)
-
-# circuit = result[1]
-# circuit.draw('mpl')
-
-counts = result[2]
-plot_histogram(counts)
+result = getPrivateKeys(0, 1, 3, 16)
+print(result)
